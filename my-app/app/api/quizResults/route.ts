fd6886beac5c1a1e56d936ce35d92d6c2358d981
Bobby <48532176+robertcs3@users.noreply.github.com>
2024-05-31 15:40:37 -0500

    sanity, quiz, stats setup.

---
+++ my-app/app/api/quizResults/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { userId, quizScore, correctAnswers, wrongAnswers } = body;
+
+  try {
+    let existingUser = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { quizResults: true },
+    });
+
+    if (existingUser && existingUser.quizResults && existingUser.quizResults.length > 0) {
+      const updatedUserStats = await prisma.quizResult.update({
+        where: { id: existingUser.quizResults[0].id },
+        data: {
+          quizScore: quizScore,
+          correctAnswers: correctAnswers,
+          wrongAnswers: wrongAnswers,
+        },
+      });
+      return NextResponse.json({ updatedUserStats });
+    } else {
+      const newUser = await prisma.user.update({
+        where: { id: userId },
+        data: {
+          quizResults: {
+            create: {
+              quizScore: quizScore,
+              correctAnswers: correctAnswers,
+              wrongAnswers: wrongAnswers,
+            },
+          },
+        },
+      });
+      return NextResponse.json({ newUser });
+    }
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
---
+++ my-app/sanity/schema.ts
@@ -0,0 +1,5 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import questions from './questions'
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [questions],
+}
